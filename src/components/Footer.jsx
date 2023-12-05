93cdced4d702168cc0423a1fab329abd1298afa1
akash0112 <akashyadav0112@gmail.com>
2023-12-05 15:28:37 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className='bg-gray-400 text-center'>Copyright 2023</div>
+  )
+}
+
+export default Footer
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='bg-gray-400 h-24 flex justify-center items-center'>Test Task</div>
+  )
+}
+
+export default Header
